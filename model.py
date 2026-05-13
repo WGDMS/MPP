6ec2f354d5ad0739fc4a7b68883de396f6a6b67e
WGDMS <101027176+WGDMS@users.noreply.github.com>
2026-05-14 00:12:28 +1200

    Add files via upload

---
+++ model.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+
+from randomwalker_atom import RandomWalkerAtomEncoder
+
+
+class GraphModel(nn.Module):
+    def __init__(
+        self,
+        n_output=1,
+        output_dim=128,
+        dropout=0.2,
+        walk_encoder="mamba",
+    ):
+        super().__init__()
+
+        self.MolAtomEncoder = RandomWalkerAtomEncoder(
+            in_node_dim=101,
+            in_edge_dim=11,
+            hidden_size=output_dim,
+            num_layers=3,
+            walk_encoder=walk_encoder,
+            walk_length=50,
+            window_size=8,
+            dropout=dropout,
+            global_pool="mean",
+            global_mp_type="vn",
+            local_mp_type="gin",
+            d_conv=9,
+            d_state=16,
+            expand=2,
+            mlp_ratio=2,
+            proj_mlp_ratio=1,
+            bidirection=True,
+            use_positional_encoding=True,
+        )
+
+        self.predictor = nn.Sequential(
+            nn.Linear(output_dim, 256),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(256, 1024),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(512, n_output),
+        )
+
+    def forward(self, data_mol):
+        graph_embedding = self.MolAtomEncoder(data_mol)
+        output = self.predictor(graph_embedding)
+        return output
